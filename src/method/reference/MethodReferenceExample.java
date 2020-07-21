63ae093df160857f5ba55f3cafb47b31409d1535
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2020-07-21 11:23:42 +0530

    practiced method references

--- src/method/reference/MethodReferenceExample.java
+++ src/method/reference/MethodReferenceExample.java
@@ -1,13 +1,17 @@
 package method.reference;
 
 
-import java.util.function.Function;c
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class MethodReferenceExample {
 
     static Function<String, String> lowerCaseFunction = String::toLowerCase;
 
+    static Consumer<String> print = System.out::println;
+
     public static void main(String[] args) {
         System.out.println(lowerCaseFunction.apply("BISHal"));
+        print.accept("BISHal");
     }
 }
